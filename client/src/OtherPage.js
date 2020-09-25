ce692c1564fb30a104cb2f552f2342412b3f138c
Yunshu <yliu@lyft.com>
2020-09-25 12:02:31 -0700

    init commit

---
+++ client/src/OtherPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default () => {
+  return (
+    <div>
+      Under development!
+      <Link to="/">Go back to home page!</Link>
+    </div>
+  );
+};
